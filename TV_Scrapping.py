f0d9b23e4f2a30fbe274c1289e5590655267d58e
divyjeet <152507238+sento-12@users.noreply.github.com>
2025-04-07 13:59:37 +0530

    Update TV_Scrapping.py

--- TV_Scrapping.py
+++ TV_Scrapping.py
@@ -16,7 +16,7 @@ from bs4 import BeautifulSoup
 chrome_options = Options()
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-extensions")
-chrome_options.add_argument("--disable-gpu")
+# chrome_options.add_argument("--disable-gpu")
 # chrome_options.add_argument("--headless")  # Run headless Chrome
 chrome_options.add_argument("user-data-dir=User Data")  
 chrome_options.add_argument("profile-directory=Default")  
